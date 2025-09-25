a1d45841e444547736d9d9093a6a68486dd108cf
Alexandre Bedel <alexandre.bedel@epitech.eu>
2025-09-25 18:48:15 +0200

    Remove unused intent

--- src/lib/discord.ts
+++ src/lib/discord.ts
@@ -1,10 +1,7 @@
 import { Client, GatewayIntentBits, REST } from "discord.js";
 import { config } from "./config";
 
-const client = new Client({
-  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages],
-});
-
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 const rest = new REST({ version: "10" }).setToken(config.discordToken);
 
 export { client as discordClient, rest };
